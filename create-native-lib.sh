012ab4a70dc02c6fe3f5ee081c4849320c9fd866
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2019-03-14 18:29:23 +0100

    remove the old build script

--- create-native-lib.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-echo 'Generating the C++ header file.';
-
-javac -h src/native/ -classpath ~/.m2/repository/org/springframework/spring-context/5.1.2.RELEASE/spring-context-5.1.2.RELEASE.jar src/main/java/fr/mim/cl/server/services/JniDirectionService.java src/main/java/fr/mim/cl/server/model/*.java src/main/java/fr/mim/cl/server/services/DirectionService.java -d target/
-
-echo 'Compiling the library.';
-
-g++ -fPIC -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/linux" -shared -o lib/libmap.so src/native/*.cpp src/native/core/*.cpp core/src/core/API/interface.cpp core/src/core/API/interface.hpp
